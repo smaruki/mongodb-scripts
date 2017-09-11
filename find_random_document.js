86a528f86a3b1a7f0164ff8fe6e4f460a23cc84c
Sergio Maruki <smaruki@users.noreply.github.com>
2017-09-11 11:08:56 -0300

    Update find_random_document.js
    Add db to script

--- find_random_document.js
+++ find_random_document.js
@@ -1,2 +1,5 @@
+/* finds a random document according to a number of records in a collection */
+db = new Mongo('localhost').getDB('test')
 collection = 'mycoll';
+
 db[collection].find().limit(-1).skip(Math.floor((Math.random() * db[collection].count()) + 1));
