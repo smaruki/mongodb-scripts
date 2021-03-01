2d14524d942b6ceae0627afede6bfa87c69a88de
Sergio Maruki <smaruki@users.noreply.github.com>
2021-03-01 17:20:11 -0300

    Create show_all_indexes.js

---
+++ show_all_indexes.js
@@ -0,0 +1,20 @@
+db.getCollectionNames().forEach(
+    function(c){
+        indexes = db.getCollection(c).getIndexes();
+        newIndexes = []
+        for(x in indexes){
+            if(indexes[x]['name'] != '_id_'){
+                delete indexes[x]['v'];
+                delete indexes[x]['ns'];
+                delete indexes[x]['background'];
+                newIndexes.push(indexes[x]);
+            }
+        }
+        print("\n/*"+c+"*/");
+        if(typeof newIndexes[0] !== 'undefined') {
+            print("db.runCommand(")
+            printjson({"createIndexes": c, "indexes": newIndexes});
+            print(");")
+        }
+    }
+)
